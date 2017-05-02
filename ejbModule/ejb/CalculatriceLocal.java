913e06ad87c864ffb11654135f8da00d1fd055b5
izardj <izard.jerome@free.fr>
2017-05-02 12:18:30 +0200

    creation d'une bean session stateless et des interfaces Local et Remote

---
+++ ejbModule/ejb/CalculatriceLocal.java
@@ -0,0 +1,13 @@
+package ejb;
+
+import javax.ejb.Local;
+
+@Local
+public interface CalculatriceLocal {
+
+	public int addition(int x, int y);
+	public int soustraction(int x, int y);
+	public int division(int x, int y);
+	public int multiplication(int x, int y);
+	
+}
